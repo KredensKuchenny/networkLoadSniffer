cbec86c643405aac15cf9a6d6d39c1035dc2e0e5
_KrEdEnS_ <61777542+KredensKuchenny@users.noreply.github.com>
2024-11-26 01:46:39 +0100

    Add files via upload

--- networkLoadSniffer.sh
+++ networkLoadSniffer.sh
@@ -4,12 +4,13 @@ INTERVAL="1"  #update interval in seconds
 NETWORKINTERFACE="ens18" #network interface
 INCOMINGTRAFFIC="40" #incoming traffic in Mibt/s
 INCOMINGTRAFFICPPS="400000" #incoming traffic in PPS/s
-CURRENTDATE=`date +%d-%m-%Y-%T` #current date (don't change it)
 DELAYAFTERIMACT="30" #delay after network pick in seconds
 NUMBEROFPACKETS="100000" #number of captured packets
 
 while true
 do
+
+        CURRENTDATE=`date +%d-%m-%Y-%T` #current date (don't change it)
 
         RP1=`cat /sys/class/net/$NETWORKINTERFACE/statistics/rx_packets` #number of packets received
         RB1=`cat /sys/class/net/$NETWORKINTERFACE/statistics/rx_bytes` #number of bytes received
